65ef08add9451696700ce4fe2ed3213c09adf8ba
Tomas Granados <tomas.granados@gmail.com>
2025-09-05 21:30:44 +0100

    3er commit

---
+++ configuration.py
@@ -0,0 +1,7 @@
+URL_SERVICE = "https://cnt-2119e9b7-58c4-438f-a5a4-0773b3aac882.containerhub.tripleten-services.com"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users"
+
+print(URL_SERVICE + USERS_TABLE_PATH)
